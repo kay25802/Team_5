a1f91551c95a7f02160c6fa9ee2dd60e449ccc54
pookey1104 <pookey1104@naver.com>
2023-11-22 08:19:47 +0900

    [1122] main 완료 #서윤

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -18,7 +18,35 @@ function Main() {
           <p>⬤⠀⠀ 티밍을 거쳐간 이름들⠀⠀ ⬤</p>
         </div>
 
-        <div className="teaming-names"></div>
+        <div className="teaming-names">
+          <div className="teaming-name">
+            <p>5랜밤 5랜밤</p>
+            <p>24시간이 모자라</p>
+            <p>남색 몽블랑</p>
+            <p>A+받고싶어</p>
+            <p>학생회입니다</p>
+            <p>Teaming</p>
+          </div>
+
+          <div className="teaming-name">
+            <p>Savior</p>
+            <p>삼시세끼</p>
+            <p>마치된것같아손오공</p>
+            <p>잠을 깨자!</p>
+            <p>점메추해줘</p>
+            <p>안도</p>
+          </div>
+
+          <div className="teaming-name">
+            <p>보915</p>
+            <p>최강컴공과</p>
+            <p>멋4</p>
+            <p>경상도</p>
+            <p>에프싫어싫어</p>
+            <p>수고하조</p>
+            <p>Dukgle</p>
+          </div>
+        </div>
 
         <div className="main-btn-title">
           <p>조 이름 짓기가 어렵다면?</p>
