a153eac59a657bb816076dc33c017af461103a05
pookey1104 <pookey1104@naver.com>
2023-11-24 05:26:45 +0900

    [1124] make+random name frontend로 #서윤

--- frontend/src/pages/MakeNameDone.js
+++ frontend/src/pages/MakeNameDone.js
@@ -1,12 +1,23 @@
-import Header from "../components/Header";
-// import "../styles/MakeNameDone.css";
+import React from 'react';
+import logoImage from "../img/about_Teaming_logo.png";
+import "../styles/MakeNameDone.css";
 
-function MakeNameDone() {
+const MakeNameDone = ({ selectedWord, onMainPage, onViewWordCloud }) => {
   return (
-    <div>
-      <Header />
+    <div className="make-name-done">
+      <div className="done-text">
+        <p>선택한 팀 이름이 워드클라우드에 등록되었어요.<br />더 많은 이름을 워드클라우드에서 확인하고, 크게 노출시켜 보세요!</p>
+      </div>
+      <div className="logo">
+        <img src={logoImage} alt="Logo" />
+      </div>
+      <div className="buttons">
+        <button onClick={onMainPage}>메인페이지로 이동</button>
+        <button onClick={onViewWordCloud}>워드클라우드 보러가기</button>
+      </div>
     </div>
   );
-}
+};
 
 export default MakeNameDone;
+
