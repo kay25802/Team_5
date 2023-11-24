466cd443d2505f1be1b977673c07bd33e6aa61a4
pookey1104 <pookey1104@naver.com>
2023-11-25 07:59:25 +0900

    [1125] 로딩 이미지 크기 조정 #서윤

--- frontend/src/components/Animation.js
+++ frontend/src/components/Animation.js
@@ -2,7 +2,7 @@ import Lottie from "react-lottie-player";
 import loadingImg from "../img/loading.json";
 
 function Animation() {
-  return <Lottie loop animationData={loadingImg} play style={{ height: "25vh" }} />;
+  return <Lottie loop animationData={loadingImg} play style={{ height: "30vh" }} />;
 }
 
 export default Animation;
